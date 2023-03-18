fa072808266c5eb2d41e290e4a62a2c3a36eb9e1
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-03-19 00:03:31 +0500

    Todo Completed

--- main/app.js
+++ main/app.js
@@ -5,27 +5,43 @@ const Add = () =>{
 let inp = document.getElementById('input');
 let txt = document.createTextNode(inp.value);
 let span = document.createElement(`span`);
+let div = document.createElement(`div`);
 let del = document.createElement(`button`);
-del.setAttribute(`onclick`,`Del()`)
+del.setAttribute(`onclick`,`Del(this)`);
+let edt = document.createElement(`button`);
+edt.setAttribute(`onclick`,`Updt(this)`);
 let logo = document.createElement(`i`);
+let logo1 = document.createElement(`i`);
 let list = document.createElement(`li`);
-list.setAttribute(`class`,`w-full flex border-2 border-cyan-700 space-x-2 text-slate-900 bg-cyan-700 `)
-del.setAttribute(`class`,`w-10 text-slate-900 opacity-0 hover:opacity-100 hover:delay-75  hover:visible hover:bg-slate-900 hover:text-cyan-700 `)
+list.setAttribute(`class`,`w-full flex border-2 justify-between border-cyan-700 space-x-2 text-slate-900 bg-cyan-700 `)
+del.setAttribute(`class`,`w-10 text-slate-900 opacity-0 hover:opacity-100 hover:duration-200 hover:delay-75  hover:visible hover:bg-slate-900 hover:text-cyan-700 `);
+edt.setAttribute(`class`,`w-10 text-slate-900 opacity-0 hover:opacity-100 hover:duration-200 hover:delay-75  hover:visible hover:bg-slate-900 hover:text-cyan-700 `);
+logo1.setAttribute(`class`,`fa-solid fa-pen-to-square`);
 logo.setAttribute(`class`,`fa-solid fa-trash-can`);
-span.setAttribute(`class`,`font-medium break-words inline-block p-2 w-60 sm:w-96 `);
+span.setAttribute(`class`,`font-medium break-words inline-block p-2 sm:w-10/12 w-7/12 `);
+div.setAttribute(`class`,`flex `);
 span.appendChild(txt);
+edt.appendChild(logo1)
 del.appendChild(logo);
 list.appendChild(span);
-list.appendChild(del);
+list.appendChild(div)
+div.appendChild(edt);
+div.appendChild(del);
 task.appendChild(list);
 inp.value = ''
+
 }
 
-// const Del = () =>{
+const Del = (rem) =>{
 
+rem.parentNode.parentNode.remove()
 
-// let rem = document.querySelector(`li`)
+}
 
-// rem.target
+const Updt = (upt) =>{
 
-// }
+ let edit =  prompt(`Enter The Updated Task :`, upt.parentNode.parentNode.firstChild.firstChild.nodeValue);
+    console.log(upt.parentNode.parentNode.firstChild.firstChild.nodeValue)
+    upt.parentNode.parentNode.firstChild.firstChild.nodeValue = edit;
+
+}
